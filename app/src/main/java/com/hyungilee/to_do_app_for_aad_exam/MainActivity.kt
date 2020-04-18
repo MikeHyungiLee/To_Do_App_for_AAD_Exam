775d922b6f717ef0417589d04d18e52d362b6d79
hyungilee <mike.hyungi.lee@gmail.com>
2020-04-18 16:59:21 +0900

    [add] Initial commit

---
+++ app/src/main/java/com/hyungilee/to_do_app_for_aad_exam/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyungilee.to_do_app_for_aad_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
